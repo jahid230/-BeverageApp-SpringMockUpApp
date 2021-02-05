f222c94e7110b1d288d23519e623c633d090f08c
jahid230 <jahid.crypto@gmail.com>
2021-02-05 23:44:14 +0100

    Spring Intialized Project

---
+++ src/main/java/com/beverageApp/beverageCore/BeverageCoreApplication.java
@@ -0,0 +1,13 @@
+package com.beverageApp.beverageCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeverageCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeverageCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beverageApp/beverageCore/BeverageCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beverageApp.beverageCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeverageCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
